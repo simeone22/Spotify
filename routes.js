07cfc53ee120782f88f9217cce45050a1f64e5c4
simeone22 <47712260+simeone22@users.noreply.github.com>
2024-05-12 12:34:16 +0200

    Added routes file to simplify the handling of express router

---
+++ routes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+export default function routes(){
+    const app = express();
+
+    //! Scrivi qui dentro tutti gli route possibili per express (Esempio: app.get('/home', (req, res) => {console.log('Sono bello!')});)
+    
+    return app;
+}
